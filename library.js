68d9bbd4eecad536b70cf38da9fc8cbbf043a806
Eric S <estriler@hotmail.com>
2023-08-08 14:39:59 -0500

    made library.js

---
+++ library.js
@@ -0,0 +1,13 @@
+class Animation {
+    constructor(title, tags, url){
+        this.title = title;
+        this.tags = [tags];
+        this.url = url;
+    }
+}
+
+const yamiFNAF2 = new Animation("YamiMash Plays FNAF 2", ["FNAF", "FNAF2", "Yamimash", "commission"], "https://youtu.be/hqw10gnZxOI");
+
+console.log(yamiFNAF2.title);
+
+ 
